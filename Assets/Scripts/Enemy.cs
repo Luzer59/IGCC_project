10e85d29888729db0bd3044f59f544b3c28e6d48
Mikko Juutinen <luzer59@hotmail.com>
2017-09-07 11:27:33 +0900

    Added previous prototype files for a template

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveXSpeed;
+    public float moveYSpeed;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float vert = Input.GetAxis("AIVertical");
+        float hor = Input.GetAxis("AIHorizontal");
+
+        rb.AddForce(Vector2.right * moveXSpeed * hor * Time.deltaTime);
+        rb.AddForce(Vector2.up * moveYSpeed * vert * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private Rigidbody2D rb;
+    private bool grounded = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        rb.AddForce(Vector2.right * moveSpeed * hor * Time.deltaTime);
+        if (vert > 0.1f && grounded)
+        {
+            rb.AddForce(Vector2.up * jumpSpeed * Time.deltaTime);
+        }
+        grounded = false;
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        grounded = true;
+    }
+}
