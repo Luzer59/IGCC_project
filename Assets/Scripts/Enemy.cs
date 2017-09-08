39249b9de18b0efa21be35ec09d522e8459e9062
Mikko Juutinen <luzer59@hotmail.com>
2017-09-08 18:08:08 +0900

    Enemy navmesh movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,9 @@ public class Enemy : MonoBehaviour
     public float maxSpeed;
     public Transform spawnPoint;
     public float decelerationDrag;
+    public MovementMode movementMode;
+
+    public enum MovementMode { Free, NavMeshAI }
 
     private Rigidbody rb;
     private NavMeshAgent navAgent;
@@ -16,35 +19,72 @@ public class Enemy : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        navAgent = GetComponent<NavMeshAgent>();
+        navAgent = GetComponent<NavMeshAgent>(); 
     }
 
     void Start()
     {
-        if (navAgent != null && rb != null)
+        switch (movementMode)
         {
-            rb.isKinematic = true;
+            case MovementMode.Free:
+                if (navAgent)
+                {
+                    navAgent.enabled = false;
+                }
+                break;
+
+            case MovementMode.NavMeshAI:
+                if (rb)
+                {
+                    rb.isKinematic = true;
+                }
+                break;
+
+            default:
+                break;
         }
     }
 
     void Update()
     {
-        if (navAgent == null && rb != null)
+        switch (movementMode)
         {
-            float vert = Input.GetAxis("Vertical2");
-            float hor = Input.GetAxis("Horizontal2");
-            Vector3 velocity = rb.velocity;
-            velocity += acceleration * new Vector3(hor, 0f, vert).normalized * Time.deltaTime;
-            if (Mathf.Approximately(vert, 0f) & Mathf.Approximately(hor, 0f))
-            {
-                velocity = Vector3.MoveTowards(velocity, Vector3.zero, decelerationDrag);
-            }
-            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-            rb.velocity = velocity;
-        }
-        if (navAgent != null)
-        {
-            
+            case MovementMode.Free:
+                if (navAgent)
+                {
+                    float vert = Input.GetAxis("Vertical2");
+                    float hor = Input.GetAxis("Horizontal2");
+                    Vector3 velocity = rb.velocity;
+                    velocity += acceleration * new Vector3(hor, 0f, vert).normalized * Time.deltaTime;
+                    if (Mathf.Approximately(vert, 0f) & Mathf.Approximately(hor, 0f))
+                    {
+                        velocity = Vector3.MoveTowards(velocity, Vector3.zero, decelerationDrag);
+                    }
+                    velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+                    rb.velocity = velocity;
+                }
+                break;
+
+            case MovementMode.NavMeshAI:
+                if (rb)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        RaycastHit hit;
+                        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                        {
+                            NavMeshHit navHit;
+                            if (NavMesh.SamplePosition(hit.point, out navHit, navAgent.height * 2, 1))
+                            {
+                                navAgent.SetDestination(navHit.position);
+                            }
+                        }
+                    }
+                }
+                break;
+
+            default:
+                break;
         }
     }
 
