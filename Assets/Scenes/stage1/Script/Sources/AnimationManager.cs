78ba2e78678e46671e595db9980395066c898267
TSITCL\s152221 <natumi.2140@gmail.com>
2017-09-12 17:04:38 +0900

    Stage1data

---
+++ Assets/Scenes/stage1/Script/Sources/AnimationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour {
+
+    [SerializeField, Header("Object")]
+    public GameObject gameobj;
+
+    [SerializeField, Header("Animator")]
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = gameobj.GetComponent<Animator>();
+    }
+
+    //PlayAnimation
+    public void PlayAnimation(string _name)
+    {
+        animator.CrossFade(_name, 0);
+    }
+
+}
