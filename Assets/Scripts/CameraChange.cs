a6bbda65de90792a91ab11b102fb18d6999c9878
kei <fkei120@yahoo.co.jp>
2017-09-13 09:43:54 +0900

    Camera Change

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+
+    public Camera MainCamera;
+    public Camera SubCamera;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        MainCamera.enabled = true;
+        SubCamera.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Change
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            MainCamera.enabled = !MainCamera.enabled;
+            SubCamera.enabled = !SubCamera.enabled;
+        }
+	}
+}
